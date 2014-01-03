8471a7edc6fbb77aaa8dbbee03a3a5aa08136ffb
Adler Medrado <adler@adlermedrado.com.br>
2014-01-03 21:33:53 -0200

    Some scripting...

---
+++ backup.ps1
@@ -0,0 +1,12 @@
+﻿#To use: ./backup.ps1 vmname path-to-backup 
+#ie.: ./backup.ps1 myvm c:\temp\myvmbackup.ovf
+
+param(
+     [Parameter(Mandatory)]
+     [string]$vm,
+     [Parameter(Mandatory)]
+     [string]$path
+ )
+
+$newName = $vm + "$("{0:yyyyMMddhhmmss}" -f (get-date)).ovf"
+iex $("VBoxManage export " + $vm + " --output " + $path + "\" + $newName)
